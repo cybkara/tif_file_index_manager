01ea70740d13e5c69433761b2d06f0d54d797530
unknown <k.cybulska07@gmail.com>
2024-07-12 20:47:05 +0200

    Read bbox and crsfrom tif with rasterio, plotting bbox using matpotlib shapely

--- tif_file_index_manager/.ipynb_checkpoints/tif_file_index_manager-checkpoint.py
+++ tif_file_index_manager/.ipynb_checkpoints/tif_file_index_manager-checkpoint.py
@@ -1,8 +1,11 @@
 def format_finder(file_format, path):
 
+    files_list = []
     if not file_format.startswith('.'):
         raise ValueError('Add dot at the beggining of the file_format')
     for root, dirs, files in os.walk(path):
         for file in files:
            if file_format in file:
-               print(os.path.join(root,file))
+               files_list.append(os.path.join(root,file))
+    
+    return files_list      
